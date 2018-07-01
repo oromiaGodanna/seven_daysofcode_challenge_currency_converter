3a7105618a9b91277930adc44f1cd5b2208fb660
oromiaGodanna <37736629+oromiaGodanna@users.noreply.github.com>
2018-07-01 16:20:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ self.addEventListener('fetch', event => {
 });
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(urlsToCache))
+    caches.open(staticCacheName).then(cache => cache.addAll(urlsToCache))
   );
 });
 self.addEventListener('activate', function(event) {
