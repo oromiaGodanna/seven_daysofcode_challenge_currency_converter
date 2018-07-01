f5a230ce938596764913f45388776c15bd3aebd4
oromiaGodanna <37736629+oromiaGodanna@users.noreply.github.com>
2018-07-01 13:25:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener('fetch', function(event){
-    console.log(event.request);
+
+let cacheName = 'cache-v1';
+let urlsToCache = [
+  './index.html',
+  './style.css',
+  './currencyConverter.js',
+];
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(urlsToCache))
+  );
 });
