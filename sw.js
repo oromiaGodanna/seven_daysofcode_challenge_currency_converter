db54ae7d589feef76afbb4f9bd484a8b19beb661
oromiaGodanna <37736629+oromiaGodanna@users.noreply.github.com>
2018-07-01 14:17:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,6 @@ let urlsToCache = [
   './currencyConverter.js',
 ];
 self.addEventListener('fetch', function(event) {
-
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
