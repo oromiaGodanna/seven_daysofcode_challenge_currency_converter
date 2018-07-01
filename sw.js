ef98e4b594afcb90509e4abaaed9be8683518e07
oromiaGodanna <37736629+oromiaGodanna@users.noreply.github.com>
2018-07-01 16:08:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,9 +7,8 @@ let urlsToCache = [
 self.addEventListener('fetch', function(event) {
 
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response =>  response || fetch(event.request);
+    )
   );
 });
 self.addEventListener('install', event => {
